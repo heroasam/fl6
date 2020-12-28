38d2a870669bee3f8f63d5380ffe05990a859ddb
heroasam <hfj024@gmail.com>
2020-12-28 12:03:26 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,181 @@
+from flask import Flask
+from flask import render_template,url_for,request,redirect, send_file,jsonify
+import psycopg2
+import psycopg2.extras
+from lib import *
+import pandas as pd
+import numpy as np
+import re
+import ast
+
+
+
+
+app = Flask(__name__)
+PORT = 5000
+DEBUG = False
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+
+
+#con = psycopg2.connect(dbname='daq6n3vvmrg79o', user='ynpqvlqqsidhga', host='ec2-3-95-87-221.compute-1.amazonaws.com', password='4bded69478ac502d5223655094cbc2241ed5aaf025f0b31fd19494c5aa35d6f0',sslmode='require')
+con = psycopg2.connect(dbname='hero', user='hero', host='localhost', password='ata', port=5432)
+
+
+
+@app.route('/pivot/pagos_cobr')
+def pivot_pagos_cobr():
+    sql="select ym(fecha) as fecha,imp+rec as cobranza,(imp+rec)*0.15 as comision,cobr from pagos where cobr in (750,815,796,800,816) and fecha>'2018-07-31'"
+    dat = pd.read_sql_query(sql, con)
+    df = pd.DataFrame(dat)
+    tbl = pd.pivot_table(df, values=['comision','cobranza'],index='fecha',columns='cobr',aggfunc='sum')
+    tbl = tbl.fillna("")
+    tbl = tbl.to_html(table_id="table",classes="table table-sm")
+    return render_template("pivot_cobr.html", tbl=tbl)
+
+
+@app.route('/pivot/retiros')
+def pivot_retiros():
+    sql="select ym(fecha) as fecha, cuenta, imp from caja where cuenta like 'retiro%'"
+    dat = pd.read_sql_query(sql, con)
+    df = pd.DataFrame(dat)
+    tbl = pd.pivot_table(df, values='imp',index='fecha',columns='cuenta',aggfunc='sum')
+    tbl = tbl.fillna("")
+    tbl = tbl.to_html(table_id="table",classes="table  table-sm")
+    return render_template("pivot_retiros.html", tbl=tbl)
+
+
+@app.route('/pivot/retiros/excel')
+def pivot_retiros_excel():
+    sql="select ym(fecha) as fecha, cuenta, imp from caja where cuenta like 'retiro%'"
+    dat = pd.read_sql_query(sql, con)
+    df = pd.DataFrame(dat)
+    tbl = pd.pivot_table(df, values='imp',index='fecha',columns='cuenta',aggfunc='sum')
+    tbl = tbl.fillna("")
+    tbl = tbl.to_excel('retiros.xlsx', index=False)
+    return send_file('retiros.xlsx')
+
+
+@app.template_filter()
+def cur(monto):
+    if monto==None:
+        return None
+    else:
+        return f"${int(monto)}"
+
+
+@app.route('/pagos')
+def pagos():
+    return render_template("pagosvue.html")
+
+@app.route('/pagos/planilla/<string:fechapago>/<int:cobrador>')
+def pagos_planilla(fechapago,cobrador):
+    planilla = pgdict(con,f"select pagos.id as id, rbo, fecha, idvta,imp::INTEGER as imp, rec::INTEGER as rec, (imp+rec)::INTEGER as total, nombre, calle||' '||num as direccion, zona,deuda::INTEGER as deuda from pagos, clientes where clientes.id=pagos.idcliente and fecha='{fechapago}' and pagos.cobr={cobrador} order by id desc")
+    lote = pgonecolumn(con,f"select lote from pagos where fecha='{fechapago}' and cobr={cobrador}") 
+    return jsonify(planilla=planilla,lote=lote)
+
+
+@app.route('/pagos/buscar/<string:cuenta>')
+def pagos_buscar(cuenta):
+    rcuenta = r'^[0-9]{5}$'
+    rdni = r'^[0-9]{7,8}$'
+    if (re.match(rcuenta,cuenta)):
+        clientes = pgdict(con,f"select nombre,calle||' '||num as direccion,dni from clientes,ventas where clientes.id=ventas.idcliente and ventas.id={cuenta}")
+    elif (re.match(rdni,cuenta)):
+        clientes = pgdict(con,f"select nombre,calle||' '||num as direccion,dni from clientes where dni='{cuenta}'")
+    else:
+        cuenta = '%'+cuenta.replace(' ','%')+'%'
+        print(cuenta)
+        clientes = pgdict(con,f"select nombre,calle||' '||num as direccion,dni from clientes where nombre||calle||num||barrio like '{cuenta}' and deuda>0")
+    print(clientes)
+    return jsonify(clientes=clientes)
+
+
+@app.route('/pagos/idvtas/<string:dni>')
+def pagos_idvtas(dni):
+    sel = f"select ventas.id as id,calle||' '||num from ventas,clientes where clientes.id=ventas.idcliente and dni='{dni}' and saldo>0"
+    print(sel)
+    idvtas = pgdict(con,sel)
+    return jsonify(idvtas=idvtas)
+
+
+@app.route('/pagos/traerficha/<int:idvta>')
+def pagos_traerficha(idvta):
+    ficha = pgdict(con,f"select id,case when saldo::integer<ic::integer then saldo::integer else ic::integer end as imp,ic::integer,saldo::integer from ventas where id={idvta}")
+    return jsonify(ficha=ficha)
+
+
+@app.route('/pagos/pasarpagos' , methods = ['POST'])
+def pagos_pasarpagos():
+    d = ast.literal_eval(request.data.decode("UTF-8"))
+    idcliente = pgonecolumn(con,f"select idcliente from ventas where id={d['idvta']}")
+    if(d['rec']==''):
+        d['rec']=0
+    ins = f"insert into pagos(idvta,fecha,imp,rec,rbo,cobr,idcliente,lote) values({d['idvta']},'{d['fecha']}',{d['imp']},{d['rec']},{d['rbo']},{d['cobr']},{idcliente},{d['lote']})"
+    print(ins)
+    cur = con.cursor()
+    cur.execute(ins)
+    con.commit()
+    trigger_pago(con, d['idvta'])
+    return 'ok'
+
+
+@app.route('/pagos/borrarpago/<int:idpago>')    
+def pagos_borrarpago(idpago):
+    stm = f"delete from pagos where id={idpago}"
+    cur = con.cursor()
+    idvta = pgonecolumn(con,f"select idvta from pagos where id={idpago}")
+    cur.execute(stm)
+    con.commit()
+    trigger_pago(con, idvta)
+    return 'ok'
+
+@app.route('/pagos/pasarplanilla', methods = ['POST'])
+def pagos_pasarplanilla():
+    d = ast.literal_eval(request.data.decode("UTF-8"))
+    idplanilla = pgonecolumn(con,f"select id from planillas where fecha = '{d['fecha']}' and idcobr={d['idcobr']}")
+    if(d['viatico']==""):
+        d['viatico']=0
+    ins = f"insert into planillas(fecha, idcobr, idlote,cobrado, comision, viatico, cntrbos) values('{d['fecha']}','{d['idcobr']}',{d['idlote']},{d['cobrado']},{d['comision']},{d['viatico']},{d['cntrbos']})"
+    upd = f"update planillas set fecha='{d['fecha']}', idcobr='{d['idcobr']}',idlote={d['idlote']},cobrado={d['cobrado']},comision={d['comision']},viatico={d['viatico']},cntrbos={d['cntrbos']} where id={idplanilla}"
+    cur = con.cursor()
+    if(idplanilla==""):
+        cur.execute(ins)
+    else:
+        cur.execute(upd)
+    con.commit()
+    return 'ok'
+
+
+@app.route('/pagos/verplanillas')
+def pagos_verplanillas():
+    return render_template("planillas.html")
+
+@app.route('/pagos/getplanillas')
+def pagos_getplanillas():
+    planillas = pgdict(con,f"select planillas.fecha as fecha,sum(cobrado::integer),sum(comision::integer),sum(viatico::integer),sum(cntrbos),(select imp::integer from caja where comentario='global' and cuenta='cobranza' and fecha=planillas.fecha) from planillas group by planillas.fecha order by planillas.fecha desc limit 100")
+    return jsonify(planillas=planillas)
+
+
+@app.route('/pagos/getplanillas/<string:fecha>')
+def pagos_getplanillashoy(fecha):
+    planillas = pgdict(con,f"select fecha,idcobr,cobrado::integer,comision::integer,viatico::integer,cntrbos,idlote from planillas where fecha='{fecha}'")
+    return jsonify(planillas=planillas)
+
+
+@app.route('/buscador')
+def buscador():
+    return render_template("buscador.html")
+
+
+@app.route('/buscador/<string:buscar>')
+def buscar_cuenta(buscar):
+    rcuenta = r'^[0-9]{5}$'
+    rdni = r'^[0-9]{7,8}$'
+    if (re.match(rcuenta,buscar)):
+        clientes = pgdict(con,f"select dni,nombre,calle||' '||num,barrio,tel,wapp,clientes.zona,clientes.pmovto,deuda::integer,sev,incobrable,gestion,subirseven,novendermas,seguir from clientes,ventas where clientes.id=ventas.idcliente and ventas.id={buscar}")
+    elif (re.match(rdni,buscar)):
+        clientes = pgdict(con,f"select dni,nombre,calle||' '||num,barrio,tel,wapp,zona,pmovto,deuda::integer,sev,incobrable,gestion,subirseven,novendermas,seguir from clientes where dni='{buscar}'")
+    else:
+        buscar = '%'+buscar.replace(' ','%')+'%'
+        clientes = pgdict(con,f"select dni,nombre,calle||' '||num from clientes where nombre||calle||num||barrio ilike '{buscar}'")
+    return jsonify(clientes=clientes) 
---
+++ lib.py
@@ -0,0 +1,315 @@
+import psycopg2
+import psycopg2.extras
+from dateutil.relativedelta import relativedelta
+
+def pgdict0(con, sel):
+    """Funcion que entrega una lista de valores en formato lista plana
+       o flat list entregado por el fetchall sobre un cursor"""
+    dict_cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    dict_cur.execute(sel)
+    rec = dict_cur.fetchone()
+    dict_cur.close()
+    return rec
+
+def pgdict(con, sel):
+    """Funcion que entrega una lista de valores en formato list of list
+       entregado por el fetchall sobre un cursor"""
+    dict_cur = con.cursor()
+    dict_cur.execute(sel)
+    rec = dict_cur.fetchall()
+    dict_cur.close()
+    return rec
+
+
+def pgonecolumn(con, sel):
+    """Funcion que entrega un solo valor como el onecolumn de sqlite
+    en caso de que el select no de resultado, lo cual se expresaria en
+    el cur.fetchone() = None damos por resultado "", sino damos [0] que
+    es el primer elemento de la tupla que normalmente fetchone entrega
+    obteniendo con eso un resultado flat para uso directo """
+    cur = con.cursor()
+    cur.execute(sel)
+    res = cur.fetchone()
+    if res is None:
+        return ""
+    else:
+        return res[0]
+    cur.close()
+    return res
+
+
+def pgddict(con, sel):
+    """Funcion que entrega una lista de valores en formato list of list
+       entregado por el fetchall sobre un cursor"""
+    dict_cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    dict_cur.execute(sel)
+    rec = dict_cur.fetchall()
+    dict_cur.close()
+    return rec
+
+
+def pgllist(con, sel):
+    cur = con.cursor()
+    cur.execute(sel)
+    res = cur.fetchall()
+    cur.close()
+    return res
+
+
+def pglflat(con,sel):
+    lista = pgllist(con,sel)
+    flatlist = []
+    for l in lista:
+        flatlist.append(l[0])
+    return flatlist
+
+
+def listsql(lista):
+    """formatea una lista para usar en un stm sqlite
+    o sea del formato (a,b,c), en especial maneja el caso
+    en que la lista de longitud 1, y entrega (a) """
+    if len(lista)==1:
+        sqllist='('+str(lista[0])+')'
+    else:
+        sqllist=str(tuple(lista))
+    return sqllist
+
+
+def strbuscar(search):
+    list = ['%']
+    if search is not None:
+        searchlist = search.split()
+    else:
+        searchlist = []
+    for s in searchlist:
+        list.append(s)
+        list.append('%')
+    return "".join(list)
+
+
+def per(periodicidad):
+    """Devuelve la periodicidad en una cadena
+    legible"""
+    if periodicidad==1:
+        return "MEN"
+    elif periodicidad==3:
+        return "SEM"
+    else:
+        return "QUIN"
+
+
+
+def desnull(cadena):
+    """filtro que elimina la cadena NULL"""
+    if cadena=="NULL":
+        return ""
+    else:
+        return cadena
+
+
+def pmovto(con,idvta):
+    ventas = pgdict0(con,f"select * from ventas where id={idvta}")
+    print(ventas)
+    print(ventas['id'])
+    pago = ventas['ent']+ventas['pagado'] if ventas['pp']==0 else ventas['ppagado']
+    icuota = ventas['ic'] if ventas['pp']==0 else ventas['pic']
+    period = ventas['p'] if ventas['pp']==0  else ventas['pper']
+    cntcuotas= ventas['cc'] if ventas['pp']==0  else ventas['pcc']
+    fechainic = ventas['primera'] if ventas['pp']==0 else ventas['pprimera']
+    enteras=round(pago/icuota)
+    if enteras==0:
+        return fechainic
+    #if {$saldo==0} {return {}} ;# este criterio de saldo cero deja pendiente el problema de los saldos negativos
+    # y la resolucion de pago>comprado no sirve para esto pq no contempla los planes de pago.
+    if ventas['saldo']<=0:
+        return None
+    # o sea si esta cancela o por algun motivo tiene saldo negativo
+    #if {$pago>=$comprado} {return {}} ;# Es erroneo en los planes de pago pq el comprado es bajo y el pago supera antes de cancelar
+    if period==1:
+        return fechainic+relativedelta(months=enteras)
+    if period==3:
+        return fechainic+relativedelta(weeks=enteras)
+    if period==2:
+        return fechainic+relativedelta(weeks=enteras*2)
+
+
+def trigger_pago(con,idvta):
+    cur = con.cursor()
+    pp = pgonecolumn(con, f"select pp from ventas where id={idvta}")
+    if pp==0:
+        upd1 = f"update ventas set pagado=COALESCE((select sum(imp) from pagos where idvta={idvta}),0) where id={idvta}"
+    else:
+        upd1 = f"update ventas set ppagado=COALESCE((select sum(imp) from pagos where idvta={idvta} and fecha>=ventas.pfecha),0) where id={idvta}"
+    upd3 = f"update ventas set ultpago=COALESCE((select max(fecha) from pagos where idvta={idvta}),NULL) where id={idvta}"
+    cur.execute(upd1)
+    cur.execute(upd3)
+    con.commit()
+    # Saldo segun si es cuenta normal o PLAN DE PAGOS
+    pp = pgonecolumn(con, f"select pp from ventas where id={idvta}")
+    if pp==0:
+        upd4 = f"update ventas set saldo=comprado-ent-pagado where id={idvta}"
+    else:
+        upd4 = f"update ventas set saldo=(pic*pcc)-ppagado where id={idvta}"
+    cur.execute(upd4)
+    con.commit()
+    # fin sector saldo
+    fpmovto = pmovto(con, idvta)
+    if fpmovto is None:
+        upd5 = f"update ventas set pmovto=NULL where id={idvta}"
+    else:
+        upd5 = f"update ventas set pmovto='{fpmovto}' where id={idvta}"
+    cur.execute(upd5)
+    con.commit()
+    idcliente = pgonecolumn(con, f"select idcliente from ventas where id={idvta}")
+    upd6 = f"update pagos set idcliente={idcliente} where idvta={idvta}"
+    upd7 = f"update clientes set deuda=COALESCE((select sum(saldo) from ventas where idcliente={idcliente}),0) where id={idcliente}"
+    upd8 = f"update clientes set pmovto=COALESCE((select max(pmovto) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    upd9 = f"update clientes set ultpago=COALESCE((select max(ultpago) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    cur.execute(upd6)
+    cur.execute(upd7)
+    cur.execute(upd8)
+    cur.execute(upd9)
+    con.commit()
+    cur.close()
+
+
+def venta_trigger(con,idvta):
+    cur = con.cursor()
+    upd1 = f"update ventas set comprado=(ic*cc) where id={idvta}"
+    idcliente = pgonecolumn(con, f"select idcliente from ventas where id={idvta}")
+    upd2 = f"update clientes set comprado=COALESCE((select sum(comprado) from ventas where idcliente={idcliente}),0) where id={idcliente}"
+    upd3 = f"update ventas set saldo=(ic*cc)-ent-pagado where id={idvta}"
+    upd4 = f"update clientes set deuda=COALESCE((select sum(saldo) from ventas where idcliente={idcliente}),0) where id={idcliente}"
+    fpmovto = pmovto(con,idvta)
+    upd5 = f"update ventas set pmovto='{fpmovto}' where id={idvta}"
+    upd6 = f"update clientes set pmovto=COALESCE((select min(pmovto) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    upd7 = f"update ventas set ultpago=fecha where id={idvta}"
+    upd8 = f"update clientes set ultcompra=COALESCE((select max(fecha) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    upd9 = f"update clientes set cuota=COALESCE((select sum(ic) from ventas where idcliente={idcliente} and saldo>0),0) where id={idcliente}"
+    cur.execute(upd1)
+    cur.execute(upd2)
+    cur.execute(upd3)
+    cur.execute(upd4)
+    cur.execute(upd5)
+    cur.execute(upd6)
+    cur.execute(upd7)
+    cur.execute(upd8)
+    cur.execute(upd9)
+    con.commit()
+    cur.close()
+
+
+
+def venta_trigger_delete(con,idcliente):
+    cur = con.cursor()
+    upd1 = f"update clientes set comprado=COALESCE((select sum(comprado) from ventas where idcliente={idcliente}),0) where id={idcliente}"
+    upd2 = f"update clientes set deuda=COALESCE((select sum(saldo) from ventas where idcliente={idcliente}),0) where id={idcliente}"
+    upd3 = f"update clientes set pmovto=COALESCE((select min(pmovto) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    upd4 = f"update clientes set ultcompra=COALESCE((select max(fecha) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    upd5 = f"update clientes set cuota=COALESCE((select sum(ic) from ventas where idcliente={idcliente} and saldo>0),0) where id={idcliente}"
+    cur.execute(upd1)
+    cur.execute(upd2)
+    cur.execute(upd3)
+    cur.execute(upd4)
+    cur.execute(upd5)
+    con.commit()
+    cur.close()
+
+
+def venta_trigger_condonada(con,idvta):
+    cur = con.cursor()
+    condonada = pgonecolumn(con, f"select condonada from ventas where id={idvta}")
+    pp = pgonecolumn(con, f"select pp from ventas where id={idvta}")
+    idcliente = pgonecolumn(con, f"select idcliente from ventas where id={idvta}")
+    if condonada==1:
+        upd1 = f"update ventas set saldo=0,pmovto=NULL where id={idvta}"
+    else:
+        fpmovto = pmovto(con, idvta)
+        if pp==0:
+            if fpmovto is None:
+                upd1 = f"update ventas set saldo=(ic*cc)-ent-pagado,pmovto=NULL where id={idvta}"
+            else:
+                upd1 = f"update ventas set saldo=(ic*cc)-ent-pagado,pmovto='fpmovto' where id={idvta}"
+        else:
+            if fpmovto is None:
+                upd1 = f"update ventas set saldo=(pic*pcc)-ppagado,pmovto=NULL where id={idvta}"
+            else:
+                upd1 = f"update ventas set saldo=(pic*pcc)-ppagado,pmovto='fpmovto' where id={idvta}"
+
+    
+    upd2 = f"update clientes set deuda=COALESCE((select sum(saldo) from ventas where idcliente={idcliente}),0) where id={idcliente}"
+    upd3 = f"update clientes set pmovto=COALESCE((select min(pmovto) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    upd4 = f"update clientes set cuota=COALESCE((select sum(ic) from ventas where idcliente={idcliente} and saldo>0),0) where id={idcliente}"
+    upd5 = f"delete from cuotas where idvta={idvta}"
+    cur.execute(upd1)
+    cur.execute(upd2)
+    cur.execute(upd3)
+    cur.execute(upd4)
+    cur.execute(upd5)
+    con.commit()
+    cur.close()
+
+
+def venta_trigger_devolucion(con,idvta):
+    cur = con.cursor()
+    idcliente = pgonecolumn(con, f"select idcliente from ventas where id={idvta}")
+    upd1 = f"update ventas set saldo=0 where id={idvta}"
+    upd2 = f"update clientes set comprado=COALESCE((select sum(comprado) from ventas where idcliente={idcliente}),0) where id={idcliente}"
+    upd3 = f"update clientes set deuda=COALESCE((select sum(saldo) from ventas where idcliente={idcliente}),0) where id={idcliente}"
+    upd4 = f"update clientes set pmovto=COALESCE((select min(pmovto) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    upd5 = f"update clientes set ultcompra=COALESCE((select max(fecha) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    upd6 = f"update clientes set cuota=COALESCE((select sum(ic) from ventas where idcliente={idcliente} and saldo>0),0) where id={idcliente}"
+    upd7 = f"update detvta set devuelta=1 where idvta={idvta}"
+    del1 = f"delete from cuotas where idvta={idvta}"
+    cur.execute(upd1)
+    cur.execute(upd2)
+    cur.execute(upd3)
+    cur.execute(upd4)
+    cur.execute(upd5)
+    cur.execute(upd6)
+    cur.execute(upd7)
+    cur.execute(del1)
+    con.commit()
+    cur.close()
+
+
+def venta_trigger_anuladevolucion(con,idvta):
+    cur = con.cursor()
+    pp = pgonecolumn(con, f"select pp from ventas where id={idvta}")
+    idcliente =  pgonecolumn(con, f"select idcliente from ventas where id={idvta}")
+    del1 = f"delete from devoluciones where idvta={idvta}"
+    del2 = f"delete from comentarios where comentario like '%DEVOLUCION%{idvta}%'"
+    upd1 = f"update ventas set devuelta=0 where id={idvta}"
+    upd2 = f"update ventas set comprado=(ic*cc) where id={idvta}"
+    upd3 = f"update clientes set comprado=COALESCE((select sum(comprado) from ventas where idcliente={idcliente}),0) where id={idcliente}"
+    if pp==0:
+        upd4 = f"update ventas set saldo=(ic*cc)-ent-pagado where id={idvta}"
+    else:
+        upd4 = f"update ventas set saldo=(pic*pcc)-ppagado where id={idvta}"
+    upd5 = f"update clientes set deuda=COALESCE((select sum(saldo) from ventas where idcliente={idcliente}),0) where id={idcliente}"
+    fpmovto = pmovto(con, idvta)
+    if fpmovto is None:
+        upd6 = f"update ventas set pmovto=NULL  where id={idvta}"
+    else:
+        upd6 = f"update ventas set pmovto='fpmovto'  where id={idvta}"
+
+    upd7 = f"update clientes set pmovto=COALESCE((select min(pmovto) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    upd8 = f"update ventas set ultpago=fecha where id={idvta}"
+    upd9 = f"update clientes set ultcompra=COALESCE((select max(fecha) from ventas where idcliente={idcliente} and devuelta=0),NULL) where id={idcliente}"
+    upd10 = f"update clientes set cuota=COALESCE((select sum(ic) from ventas where idcliente={idcliente} and saldo>0),0) where id={idcliente}"
+    upd11 = f"update detvta set devuelta=0 where idvta={idvta}"
+    cur.execute(del1)
+    cur.execute(del2)
+    cur.execute(upd1)
+    cur.execute(upd2)
+    cur.execute(upd3)
+    cur.execute(upd4)
+    cur.execute(upd5)
+    cur.execute(upd6)
+    cur.execute(upd7)
+    cur.execute(upd8)
+    cur.execute(upd9)
+    cur.execute(upd10)
+    cur.execute(upd11)
+    con.commit()
+    cur.close()
