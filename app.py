625894d7976074dbea8a6e956f6caff5102e230d
heroasam <hfj024@gmail.com>
2020-12-30 15:10:50 -0300

    boton wapp

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ DEBUG = False
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 
 
-con = psycopg2.connect(dbname='daq6n3vvmrg79o', user='ynpqvlqqsidhga', host='ec2-3-95-87-221.compute-1.amazonaws.com', password='4bded69478ac502d5223655094cbc2241ed5aaf025f0b31fd19494c5aa35d6f0',sslmode='require')
-#con = psycopg2.connect(dbname='hero', user='hero', host='localhost', password='ata', port=5432)
+# con = psycopg2.connect(dbname='daq6n3vvmrg79o', user='ynpqvlqqsidhga', host='ec2-3-95-87-221.compute-1.amazonaws.com', password='4bded69478ac502d5223655094cbc2241ed5aaf025f0b31fd19494c5aa35d6f0',sslmode='require')
+con = psycopg2.connect(dbname='hero', user='hero', host='localhost', password='ata', port=5432)
 
 
 
@@ -225,3 +225,10 @@ def buscar_fecharpmovto(dni,pmovto):
 def buscar_imprimirficha(dni):
     ficha(con,dni)
     return send_file('ficha.pdf')
+
+
+@app.route('/buscador/datosultvta/<string:dni>')
+def buscar_datosultvta(dni):
+    idcliente = pgonecolumn(con,f"select id from clientes where dni='{dni}'")
+    ultvta = pgdict(con,f"select fecha, (select max(art) from detvta where idvta=ventas.id) from ventas where idcliente={idcliente} order by id desc")
+    return jsonify(ultvta=ultvta)
