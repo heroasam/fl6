44bd6d10cf2851c9415aa8cf38a1958907426c09
heroasam <hfj024@gmail.com>
2021-05-18 10:53:32 -0300

    csrf

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, json
 from flask import render_template,url_for,request,redirect, send_file,jsonify, make_response
+from flask_wtf.csrf import CSRFProtect
 import psycopg2
 import psycopg2.extras
 from lib import *
@@ -17,6 +18,7 @@ PORT = 5000
 DEBUG = False
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 
+csrf = CSRFProtect(app)
 
 #con = psycopg2.connect(dbname='daq6n3vvmrg79o', user='ynpqvlqqsidhga', host='ec2-3-95-87-221.compute-1.amazonaws.com', password='4bded69478ac502d5223655094cbc2241ed5aaf025f0b31fd19494c5aa35d6f0',sslmode='require')
 con = psycopg2.connect(dbname='hero', user='hero', host='localhost', password='ata', port=5432)
