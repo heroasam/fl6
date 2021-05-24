e6607f40070ce28869b7df3d5a53c50021264351
heroasam <hfj024@gmail.com>
2021-05-24 19:15:16 -0300

    pongo secret key fija

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ import os
 app = Flask(__name__)
 #PORT = 5000
 #DEBUG = False
-app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
-#'7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
+
 
 #csrf = CSRFProtect(app)
 login = LoginManager(app)
