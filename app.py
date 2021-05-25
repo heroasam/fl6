265283c9f25dacdf76fde53c5e175ac01f36bf32
heroasam <hfj024@gmail.com>
2021-05-25 11:35:52 -0300

    agregado cors y csrf

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, json
 from flask import render_template,url_for,request,redirect, send_file,jsonify, make_response, session
-#from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 from flask_bcrypt import Bcrypt
 from flask_login import UserMixin
+from flask_cors import CORS
 from werkzeug.urls import url_parse
 import psycopg2
 import psycopg2.extras
@@ -23,9 +24,9 @@ app = Flask(__name__)
 #DEBUG = False
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+CORS(app)
 
-
-#csrf = CSRFProtect(app)
+csrf = CSRFProtect(app)
 login = LoginManager(app)
 login.login_view = "login"
 bcrypt = Bcrypt(app)
@@ -86,6 +87,9 @@ def login():
 
 @app.route('/logout')
 def logout():
+    global current
+    logout_user()
+    current = None
     return redirect(url_for('login'))
 
 
