f359dde7322bfcae69d4c054ddcd8773bf483d5b
heroasam <hfj024@gmail.com>
2021-05-28 16:03:26 -0300

    gestion de calles barrios y zonas

--- ventas.py
+++ ventas.py
@@ -186,3 +186,198 @@ def ventas_guardaredicionvta():
         con.commit()
         cur.close()
         return 'OK'   
+
+@ventas.route('/ventas/clientes')
+def ventas_clientes():
+    return render_template('ventas/clientes.html')
+
+
+@ventas.route('/ventas/getclientes')
+def ventas_getclientes():
+    clientes = pgdict(con, f"select ventas.id, nombre, calle,num, zona, gestion, mudo, incobrable,acla from ventas, clientes where ventas.idcliente=clientes.id order by ventas.id desc limit 200")
+    return jsonify(clientes=clientes)
+
+
+@ventas.route('/ventas/calles')
+def ventas_calles():
+    return render_template('ventas/calles.html')
+
+
+@ventas.route('/ventas/barrios')
+def barrios_calles():
+    return render_template('ventas/barrios.html')
+
+
+@ventas.route('/ventas/zonas')
+def zonas_calles():
+    return render_template('ventas/zonas.html')
+
+
+@ventas.route('/ventas/getcallesconid')
+def ventas_getcallesconid():
+    calles = pgdict(con, f"select id,calle from calles order by calle")
+    return jsonify(calles= calles)
+
+
+@ventas.route('/ventas/getbarriosconid')
+def ventas_getbarriosconid():
+    barrios = pgdict(con, f"select id,barrio from barrios order by barrio")
+    return jsonify(barrios= barrios)
+
+
+@ventas.route('/ventas/getzonasconid')
+def ventas_getzonasconid():
+    zonas = pgdict(con, f"select id,zona from zonas order by zona")
+    return jsonify(zonas= zonas)
+
+
+@ventas.route('/ventas/guardaredicioncalle', methods=['POST'])
+def ventas_guardaredicioncalle():
+    d = ast.literal_eval(request.data.decode("UTF-8"))
+    upd = f"update calles set calle='{d['calle']}' where id={d['id']}"
+    cur = con.cursor()
+    try:
+        cur.execute(upd)
+    except psycopg2.Error as e:
+        con.rollback()
+        error = e.pgerror
+        return make_response(error,400)
+    else:
+        con.commit()
+        cur.close()
+        return 'OK'   
+
+
+
+@ventas.route('/ventas/guardaredicionbarrio', methods=['POST'])
+def ventas_guardaredicionbarrio():
+    d = ast.literal_eval(request.data.decode("UTF-8"))
+    upd = f"update barrios set barrio='{d['barrio']}' where id={d['id']}"
+    cur = con.cursor()
+    try:
+        cur.execute(upd)
+    except psycopg2.Error as e:
+        con.rollback()
+        error = e.pgerror
+        return make_response(error,400)
+    else:
+        con.commit()
+        cur.close()
+        return 'OK'    
+
+
+@ventas.route('/ventas/guardaredicionzona', methods=['POST'])
+def ventas_guardaredicionzona():
+    d = ast.literal_eval(request.data.decode("UTF-8"))
+    upd = f"update zonas set zona='{d['zona']}' where id={d['id']}"
+    cur = con.cursor()
+    try:
+        cur.execute(upd)
+    except psycopg2.Error as e:
+        con.rollback()
+        error = e.pgerror
+        return make_response(error,400)
+    else:
+        con.commit()
+        cur.close()
+        return 'OK'   
+
+
+@ventas.route('/ventas/borrarcalle/<int:id>')  
+def ventas_borrarcalle(id):
+    stm = f"delete from calles where id={id}"
+    cur = con.cursor()
+    try:
+        cur.execute(stm)
+    except psycopg2.Error as e:
+        con.rollback()
+        error = e.pgerror
+        return make_response(error,400)
+    else:
+        con.commit()
+        cur.close()
+        return 'OK'
+
+
+
+@ventas.route('/ventas/borrarbarrio/<int:id>')  
+def ventas_borrarbarrio(id):
+    stm = f"delete from barrios where id={id}"
+    cur = con.cursor()
+    try:
+        cur.execute(stm)
+    except psycopg2.Error as e:
+        con.rollback()
+        error = e.pgerror
+        return make_response(error,400)
+    else:
+        con.commit()
+        cur.close()
+        return 'OK'
+
+
+@ventas.route('/ventas/borrarzona/<int:id>')  
+def ventas_borrarzona(id):
+    stm = f"delete from zonas where id={id}"
+    cur = con.cursor()
+    try:
+        cur.execute(stm)
+    except psycopg2.Error as e:
+        con.rollback()
+        error = e.pgerror
+        return make_response(error,400)
+    else:
+        con.commit()
+        cur.close()
+        return 'OK'
+
+@ventas.route('/ventas/guardarcallenueva', methods=['POST'])
+def ventas_guardarcallenueva():
+    d = ast.literal_eval(request.data.decode("UTF-8"))
+    ins = f"insert into calles(calle) values('{d['calle']}')"
+    cur = con.cursor()
+    try:
+        cur.execute(ins)
+    except psycopg2.Error as e:
+        con.rollback()
+        error = e.pgerror
+        return make_response(error,400)
+    else:
+        con.commit()
+        cur.close()
+        return 'OK'
+
+
+
+@ventas.route('/ventas/guardarbarrionueva', methods=['POST'])
+def ventas_guardarbarrionueva():
+    d = ast.literal_eval(request.data.decode("UTF-8"))
+    ins = f"insert into barrios(barrio) values('{d['barrio']}')"
+    cur = con.cursor()
+    try:
+        cur.execute(ins)
+    except psycopg2.Error as e:
+        con.rollback()
+        error = e.pgerror
+        return make_response(error,400)
+    else:
+        con.commit()
+        cur.close()
+        return 'OK'
+
+
+@ventas.route('/ventas/guardarzonanueva', methods=['POST'])
+def ventas_guardarzonanueva():
+    d = ast.literal_eval(request.data.decode("UTF-8"))
+    ins = f"insert into zonas(zona) values('{d['zona']}')"
+    cur = con.cursor()
+    try:
+        cur.execute(ins)
+    except psycopg2.Error as e:
+        con.rollback()
+        error = e.pgerror
+        return make_response(error,400)
+    else:
+        con.commit()
+        cur.close()
+        return 'OK'
