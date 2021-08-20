c04b0acdb8d5867221315262b992c7282b61855f
heroasam <hfj024@gmail.com>
2021-08-20 19:39:40 -0300

    soluciono el tema Morosidad
    era debido a que no estaba la columna vencido

--- con.py
+++ con.py
@@ -5,10 +5,10 @@ import mysql.connector
 
 
 #con = pymysql.connect(host='localhost',database='hero',user='hero',password='ata')
-con = mysql.connector.connect(host='localhost',database='hero',user='hero',password='ata')
+con = mysql.connector.connect(host='localhost',database='hero',user='hero',password='ataH2132**/')
 def get_con():
     #con = pymysql.connect(host='localhost',database='hero',user='hero',password='ata')
-    con = mysql.connector.connect(host='localhost',database='hero',user='hero',password='ata')
+    con = mysql.connector.connect(host='localhost',database='hero',user='hero',password='ataH2132**/')
     return con
 
 
