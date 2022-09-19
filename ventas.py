a9e313a0ca7bebe407ff25717cd2aaa36d295349
heroasam <hfj024@gmail.com>
2022-09-19 13:09:30 -0300

    hoy

--- ventas.py
+++ ventas.py
@@ -171,7 +171,7 @@ def ventas_borrardetvta(id):
 @ventas.route('/ventas/getarticulos')
 def ventas_getarticulos():
     con = get_con()
-    articulos = pglflat(con, f"select concat(codigo,'-',art) as art from articulos where activo=1")
+    articulos = pglflat(con, f"select concat(codigo,'-',art) as art from articulos where activo=1 and codigo is not null")
     print(articulos)
     con.close()
     return jsonify(articulos=articulos)
